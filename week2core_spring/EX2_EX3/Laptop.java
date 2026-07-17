ba07b210cca54db86efbd6d58b3f0e05d65cd510
H sree Narayane <ammusree272005@gmail.com>
2026-07-17 20:55:55 +0530

    Add files via upload

---
+++ week2core_spring/EX2_EX3/Laptop.java
@@ -0,0 +1,12 @@
+public class Laptop {
+
+    private String brand;
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public void show() {
+        System.out.println("Laptop Brand : " + brand);
+    }
+}
---
+++ week2core_spring/EX2_EX3/Main.java
@@ -0,0 +1,15 @@
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        ApplicationContext context =
+                new ClassPathXmlApplicationContext("applicationContext.xml");
+
+        Laptop l = (Laptop) context.getBean("lap");
+
+        l.show();
+    }
+}
