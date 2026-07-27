fabed5a32c709b85b4046f5cc3234ae395d410ce
H sree Narayane <ammusree272005@gmail.com>
2026-07-27 10:25:13 +0530

    Add files via upload

---
+++ SingletonPatternExample.java
@@ -0,0 +1,30 @@
+class Logger {
+
+    private static Logger obj = new Logger();
+
+    private Logger() {}
+
+    public static Logger getInstance() {
+        return obj;
+    }
+
+    void display() {
+        System.out.println("Logger is working...");
+    }
+}
+
+public class SingletonPatternExample {
+
+    public static void main(String[] args) {
+
+        Logger l1 = Logger.getInstance();
+        Logger l2 = Logger.getInstance();
+
+        l1.display();
+
+        if (l1 == l2)
+            System.out.println("Only one Logger instance created.");
+        else
+            System.out.println("Multiple instances created.");
+    }
+}
