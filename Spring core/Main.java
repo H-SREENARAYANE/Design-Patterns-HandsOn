02b505a54c28c8b6dee411b5b19f61ce986a5ce9
H sree Narayane <ammusree272005@gmail.com>
2026-07-27 13:37:56 +0530

    Add files via upload

---
+++ Spring core/Main.java
@@ -0,0 +1,15 @@
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        ApplicationContext context =
+        new ClassPathXmlApplicationContext("applicationContext.xml");
+
+        Student s = (Student) context.getBean("student");
+
+        s.display();
+    }
+}
