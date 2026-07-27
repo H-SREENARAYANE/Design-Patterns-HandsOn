767c6598e1d8000b7ee0b3c338f73d2d06d8ceee
H sree Narayane <ammusree272005@gmail.com>
2026-07-27 10:09:15 +0530

    Add files via upload

---
+++ week6GITHOL/MANDATORY_PROBLEMS/ALGORITHMS_DATASTRUCTURES/FinancialForecast.java
@@ -0,0 +1,19 @@
+public class FinancialForecast {
+
+    static double futureValue(double amount, double rate, int years) {
+        if (years == 0)
+            return amount;
+
+        return futureValue(amount * (1 + rate), rate, years - 1);
+    }
+
+    public static void main(String[] args) {
+        double presentValue = 10000;
+        double growthRate = 0.10;
+        int years = 5;
+
+        double result = futureValue(presentValue, growthRate, years);
+
+        System.out.printf("Future Value after %d years = %.2f", years, result);
+    }
+}
